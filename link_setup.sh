23f0b461a55ecb4af3ad706de26ebe8ed64c8f24
botan <sunnycastle0901@gmail.com>
2026-09-13 01:07:37 +0900

    modified .sh

--- link_setup.sh
+++ link_setup.sh
@@ -1,23 +1,29 @@
 ip netns add node1
 ip netns add node2
 ip netns add node3
+ip netns add User
 ip link add veth_1 type veth peer veth_gw1
 ip link add veth_2 type veth peer veth_gw2
 ip link add veth_3 type veth peer veth_gw3
+ip link add veth_U type veth peer veth_gw4
 ip link add ISP type bridge
 ip link set veth_gw1 master ISP
 ip link set veth_gw2 master ISP
 ip link set veth_gw3 master ISP
+ip link set veth_gw4 master ISP
 ip link set veth_1 netns node1
 ip link set veth_2 netns node2
 ip link set veth_3 netns node3
+ip link set veth_U netns User
 ip netns exec node1 ip link set veth_1 up
 ip netns exec node2 ip link set veth_2 up
 ip netns exec node3 ip link set veth_3 up
+ip netns exec User ip link set veth_U up
 ip link set ISP up
 ip link set veth_gw1 up
 ip link set veth_gw2 up
 ip link set veth_gw3 up
+ip link set veth_gw4 up
 ip addr add 10.0.0.4/24 dev ISP
 ip netns exec node1 ip addr add 10.0.0.1/24 dev veth_1
 ip netns exec node2 ip addr add 10.0.0.2/24 dev veth_2
@@ -26,3 +32,4 @@ ip link show
 ip netns exec node1 ip link show
 ip netns exec node2 ip link show
 ip netns exec node3 ip link show
+ip netns exec User ip link show
